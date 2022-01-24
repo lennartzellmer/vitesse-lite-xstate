ccc4717626b3a3f532dcc200630057ce6118b121
Lennart Zellmer <lennart@zellmer.dev>
2022-01-24 02:14:01 +0100

    init

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+// register vue composition api globally
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import routes from 'virtual:generated-pages'
+import { inspect } from '@xstate/inspect'
+import App from './App.vue'
+
+import '@unocss/reset/tailwind.css'
+import './styles/main.css'
+import 'uno.css'
+
+if (import.meta.env.DEV) {
+  inspect({
+    iframe: false,
+  })
+}
+
+const app = createApp(App)
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+app.use(router)
+app.mount('#app')
